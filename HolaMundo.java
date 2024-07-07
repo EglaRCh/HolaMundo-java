337e0bfc0b8a49849c5626bd6974fb74c10693e9
EglaRCh <125770090+EglaRCh@users.noreply.github.com>
2024-07-06 21:02:41 -0600

    Update HolaMundo.java

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,7 +1,7 @@
 public class HolaMundo {
 
   public static void main(String[] args) {
-    System.out.println("Actualización automática en Jenkins");
+    System.out.println("¡Hola Mundo!");
   }
 
 }
